fb2ddbd71e3fc8bb810f0396fd74c32dd627b2de
Supertinypeanut <1176032017@qq.com>
2020-10-29 23:51:49 +0800

    style:调整组件结构

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <nav>
     <span>导航：</span>
     <router-link to="/">首页</router-link>
-    <router-link to="/content">内容</router-link>
+    <router-link to="/doc">内容</router-link>
   </nav>
   <router-view />
 </template>
 
 <script>
 export default {
-  name: "App",
-};
+  name: 'App',
+}
 </script>
